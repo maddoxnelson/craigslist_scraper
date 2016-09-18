ba5672b599893f275ef2a51c0b33c592285f2f66
Matt Nelson <maddoxnelson@gmail.com>
2016-09-17 20:45:44 -0400

    code cleanup

--- scrape.py
+++ scrape.py
@@ -1,25 +1,15 @@
-from bs4 import BeautifulSoup
-
 import sys
-
 import json
+import requests
 
 from sys import argv
 script, place, rent = argv
 
-import requests
-
 url = 'https://washingtondc.craigslist.org/jsonsearch/apa/?query=' + place
-
 rent = int(rent)
 
-i = 0
 numberOfResults = 0
-
-checkForRent = rent > 0
-
 page = requests.get(url)
-
 contents = json.loads(str(page.content))
 
 for item in contents:
@@ -28,11 +18,13 @@ for item in contents:
 		if "PostingTitle" in json.loads(post):
 			numberOfResults += 1
 			print json.loads(post)["PostingTitle"]
-			print json.loads(post)["PostingURL"]
-			print '$' + str(json.loads(post)["Ask"])
-			if int(json.loads(post)["Ask"]) <= rent:
-				print "ALERT!! THIS IS IN YOUR BUDGET!"
-				print u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525'
+			if "PostingURL" in json.loads(post):
+				print json.loads(post)["PostingURL"]
+			if "Ask" in json.loads(post):
+				print '$' + str(json.loads(post)["Ask"])
+				if int(json.loads(post)["Ask"]) <= rent:
+					print "ALERT!! THIS IS IN YOUR BUDGET!"
+					print u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525',u'\U0001f525'
 			print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
 print "Complete! Found " + str(numberOfResults) + " new results in " + place + "."
